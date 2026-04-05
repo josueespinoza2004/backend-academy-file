ba726521422a6f954cd57b9cedbb89d3e55f3f69
Josué Espinoza <142639857+josueespinoza2004@users.noreply.github.com>
2026-04-04 23:13:30 -0600

    feat: update File entity to include additional fields and adjust column properties

--- src/modules/files/entities/file.entity.ts
+++ src/modules/files/entities/file.entity.ts
@@ -1,9 +1,29 @@
 import { Column, Entity } from 'typeorm';
+import { PrimaryGeneratedColumn } from 'typeorm/browser';
 
 @Entity('files.file')
 export class File {
+  @PrimaryGeneratedColumn('increment')
   id: number;
 
-  @Column({ type: 'varchar', nullable: true, length: 100 })
-  files: string;
+  @Column({ type: 'int4', nullable: false })
+  model_id: number;
+
+  @Column({ type: 'int8', nullable: false })
+  user_id: number;
+
+  @Column({ type: 'int8', nullable: false })
+  user_updated_id: number;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updated_at: Date;
+
+  @Column({ type: 'varchar', nullable: false, length: 255 })
+  mime: string;
+
+  @Column({ type: 'varchar', nullable: false, length: 255 })
+  file_name: string;
 }
